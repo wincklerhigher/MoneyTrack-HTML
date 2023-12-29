ce47e4767ffeaf937fd19738d82edbd2fdf749c9
WillianWinckler <73580555+wincklerhigher@users.noreply.github.com>
2023-12-29 00:35:56 -0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var defaultTab = document.querySelector(".nav ul li a");
+    defaultTab.classList.add("active");
+    
+    var tabLinks = document.querySelectorAll(".nav ul li a");
+    tabLinks.forEach(function (tabLink) {
+        tabLink.addEventListener("click", function (event) {
+            changeTab(event, tabLink.getAttribute("data-tab-id"));
+        });
+    });
+});
+
+function changeTab(event, tabId) {
+    var tabContent = document.querySelectorAll(".tab-content");
+    tabContent.forEach(function (tab) {
+        tab.style.display = "none";
+    });
+
+    var tabLinks = document.querySelectorAll(".nav ul li a");
+    tabLinks.forEach(function (link) {
+        link.classList.remove("active");
+    });
+
+    document.getElementById(tabId).style.display = "block";
+    
+    event.currentTarget.classList.add("active");
+}
