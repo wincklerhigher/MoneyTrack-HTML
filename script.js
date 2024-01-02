51f997679a2ddebcbe2fbd0b30b048ce7dc9d7c5
WillianWinckler <73580555+wincklerhigher@users.noreply.github.com>
2024-01-01 23:55:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+function changeTab(event, tabId) {
+        var i, tabContent, tabLinks;
+
+        tabContent = document.querySelectorAll(".tab-content");
+        for (i = 0; i < tabContent.length; i++) {
+            tabContent[i].style.display = "none";
+        }
+
+        tabLinks = document.querySelectorAll("nav ul li a");
+        for (i = 0; i < tabLinks.length; i++) {
+            tabLinks[i].classList.remove("active");
+        }
+
+        document.getElementById(tabId).style.display = "block";
+
+        event.currentTarget.classList.add("active");
+    }
+
 document.addEventListener("DOMContentLoaded", function () {
     var defaultTab = document.querySelector(".nav ul li a");
     defaultTab.classList.add("active");
