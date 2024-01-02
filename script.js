db6f7684b1326e9d0a01c916af56eb58e5b29f55
WillianWinckler <73580555+wincklerhigher@users.noreply.github.com>
2024-01-01 23:56:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
-function changeTab(event, tabId) {
-        var i, tabContent, tabLinks;
-
-        tabContent = document.querySelectorAll(".tab-content");
-        for (i = 0; i < tabContent.length; i++) {
-            tabContent[i].style.display = "none";
-        }
-
-        tabLinks = document.querySelectorAll("nav ul li a");
-        for (i = 0; i < tabLinks.length; i++) {
-            tabLinks[i].classList.remove("active");
-        }
-
-        document.getElementById(tabId).style.display = "block";
-
-        event.currentTarget.classList.add("active");
-    }
-
 document.addEventListener("DOMContentLoaded", function () {
     var defaultTab = document.querySelector(".nav ul li a");
     defaultTab.classList.add("active");
-    
+
     var tabLinks = document.querySelectorAll(".nav ul li a");
     tabLinks.forEach(function (tabLink) {
         tabLink.addEventListener("click", function (event) {
@@ -40,23 +22,23 @@ function changeTab(event, tabId) {
     });
 
     document.getElementById(tabId).style.display = "block";
-    
+
     event.currentTarget.classList.add("active");
 }
 
 function handleFormSubmission(form) {
-        console.log("Função handleFormSubmission chamada.");
-        
-        var ServerResponse = {
-            success: true,
-            message: "Registro bem-sucedido!"
-        };
+    console.log("Função handleFormSubmission chamada.");
 
-        if (ServerResponse.success) {
-            alert(ServerResponse.message);
-            return true; 
-        } else {
-            alert("Erro no registro. Tente novamente.");
-            return false;
-        }
+    var ServerResponse = {
+        success: true,
+        message: "Registro bem-sucedido!"
+    };
+
+    if (ServerResponse.success) {
+        alert(ServerResponse.message);
+        return true;
+    } else {
+        alert("Erro no registro. Tente novamente.");
+        return false;
     }
+}
